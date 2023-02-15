35c88ca197c108b9008172c377623ce86b3f2cbf
Miquelane Melo <miquelanepmelo@gmail.com>
2023-02-15 17:21:14 -0300

    implementando findAll e findById

---
+++ 05_firstSteps/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/udemy/services/PersonServices.java
@@ -0,0 +1,55 @@
+package br.com.udemy.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.udemy.model.Person;
+
+@Service
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+
+		return persons;
+
+	}
+
+	public Person findById(String id) {
+
+		logger.info("Finding one person!!");
+
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Miquelane");
+		person.setLastName("melo");
+		person.setAddress("Fortaleza-CE");
+		person.setGender("Male");
+		return person;
+	}
+
+	private Person mockPerson(int i) {
+
+		logger.info("Finding all people!");
+
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brazil " + i);
+		person.setGender("Male");
+
+		return person;
+	}
+}
